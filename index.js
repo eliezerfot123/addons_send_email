580bd2270e80c3f712aa23c10f1d14e47884a336
Eliezer J Romero C <eliezerfot123@gmail.com>
2015-07-11 23:46:27 -0430

    Añadiendo estilo al panel con mdl

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-var self = require('sdk/self');
+var { ToggleButton } = require('sdk/ui/button/toggle');
+var panels = require("sdk/panel");
+var self = require("sdk/self");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+var button = ToggleButton({
+  id: "my-button",
+  label: "my button",
+  icon: {
+    "16": "./images/icon_16.png",
+    "32": "./images/icon_32.png",
+    "64": "./images/icon_64.png"
+  },
+  onChange: handleChange
+});
+
+var panel = panels.Panel({
+  contentURL: self.data.url("panel.html"),
+  onHide: handleHide,
+  width:512,
+  height: 300
+});
+
+function handleChange(state) {
+  if (state.checked) {
+    panel.show({
+      position: button
+    });
+  }
 }
 
-exports.dummy = dummy;
+function handleHide() {
+  button.state('window', {checked: false});
+}
